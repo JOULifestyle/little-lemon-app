abae6a343d5b3174b7eb89883586b58b56a9aa6a
Israel Olasehinde <olasehindeisrael@gmail.com>
2025-07-12 23:08:45 +0100

    Added components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import Header from './component/Header';
+import Nav from './component/Nav';
+import Main from './component/Main';
+import Footer from './component/Footer';
 
 function App() {
   return (
-    <div className="App">
-    Homepage
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/component/Header.js
@@ -0,0 +1,7 @@
+function Header() {
+    return <header>
+        <img src="Logo.svg" alt="Little Lemon Logo" />
+    </header>
+}
+
+export default Header;
